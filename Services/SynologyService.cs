3b25bce1a2762efda91c108a803a02db51b9da39
ryu1uga <20211953@aloe.ulima.edu.pe>
2026-06-01 15:05:29 -0500

    version

--- Services/SynologyService.cs
+++ Services/SynologyService.cs
@@ -33,7 +33,7 @@ namespace CorrePalabras.Services
             if (!string.IsNullOrEmpty(_cachedSid) && DateTime.UtcNow < _sidExpiration)
                 return _cachedSid;
 
-            string url = $"{_synologyBaseUrl}/webapi/auth.cgi?api=SYNO.API.Auth&version=6&method=login&account={Uri.EscapeDataString(_username)}&passwd={Uri.EscapeDataString(_password)}&session=FileStation&format=sid&enable_syno_token=yes";
+            string url = $"{_synologyBaseUrl}/webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account={Uri.EscapeDataString(_username)}&passwd={Uri.EscapeDataString(_password)}&session=FileStation&format=sid&enable_syno_token=yes";
 
             var rawResponse = await _httpClient.GetStringAsync(url);
             Console.WriteLine($"=== AUTH BODY: {rawResponse} ===");  // 👈
