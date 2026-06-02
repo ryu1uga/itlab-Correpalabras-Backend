9d55f21d76402b9ca78391b902ac3ec6abec2205
ryu1uga <20211953@aloe.ulima.edu.pe>
2026-06-02 11:47:31 -0500

    fixed

--- Services/SynologyService.cs
+++ Services/SynologyService.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.WebUtilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
