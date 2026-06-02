91781221dc3f7a701802665c2eed852818a4d0f2
ryu1uga <20211953@aloe.ulima.edu.pe>
2026-06-02 10:05:14 -0500

    -filename

--- Services/SynologyService.cs
+++ Services/SynologyService.cs
@@ -172,7 +172,6 @@ namespace CorrePalabras.Services
                 content.Add(new StringContent(targetPath), "path");
                 content.Add(new StringContent("true"), "overwrite");
                 content.Add(new StringContent("true"), "create_parents");
-                content.Add(new StringContent("true"), "filename");
 
                 using var streamContent = new StreamContent(file.OpenReadStream());
                 streamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(
